1a9308cc931eaf0840066f07db05a6fa7f39eacf
Cameron <c03cost@siena.edu>
2021-03-12 10:21:52 -0500

    Edited the class to include other classes that were changed or newly made.

--- IRSystem.java
+++ IRSystem.java
@@ -6,6 +6,9 @@ public class IRSystem {
         String directory = "TrainingData/";
         ArrayList<Document> docs = new ArrayList<Document>();
         docs = Parser.trecParser(directory);
+        Tokenization tokens = new Tokenization();
+        ArrayList<String> newTokens = tokens.tokenize(docs);
+        System.out.println(newTokens.get(1));
         System.out.println(docs.get(0).getDocID());
         System.out.println(docs.get(0).getText());
 
