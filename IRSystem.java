3b8e59c9ff1426393b29bbfc1e322ecd4910e468
larschristensen22 <larsc1222@gmail.com>
2021-03-12 09:21:46 -0500

    Added method calls to main class to test parsing.

--- IRSystem.java
+++ IRSystem.java
@@ -1,5 +1,13 @@
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
 public class IRSystem {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws FileNotFoundException {
+        String directory = "TrainingData/";
+        ArrayList<Document> docs = new ArrayList<Document>();
+        docs = Parser.trecParser(directory);
+        System.out.println(docs.get(0).getDocID());
+        System.out.println(docs.get(0).getText());
+
     }
 }
