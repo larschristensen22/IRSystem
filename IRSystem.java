e4cb61f18e8897ff793987b970d119b78740acf7
larschristensen22 <larsc1222@gmail.com>
2021-03-19 16:42:24 -0400

    Added to main class to include inverted index.

--- IRSystem.java
+++ IRSystem.java
@@ -9,24 +9,36 @@ public class IRSystem {
 
         Parser parser = new Parser();
         ArrayList<Document> docs = new ArrayList<Document>();
-        Document doc;
 
         Tokenization tokens = new Tokenization();
-        ArrayList<String> newTokens;
+        ArrayList<String> newTokens = new ArrayList<String>();
         Normalization normalize = new Normalization();
         File directoryPath = new File(directory);
         File filesList[] = directoryPath.listFiles();
+        InvertedIndex index = new InvertedIndex();
+
 
         for (File file: filesList){
             System.out.println("filename: "+ file.toString());
             docs = parser.trecParser(file.toString());
-            for (int i = 0; i < docs.size(); i++) {
+            for (int i = 0; i < 1; i++) {
                 newTokens = tokens.tokenize(docs.get(i));
                 newTokens = normalize.removeStopWords(newTokens);
+                index = index.createIndex(newTokens, docs.get(i).getDocID());
             }
 
         }
 
+        // for (int i = 0; i < newTokens.size(); i++) {
+        //     System.out.println("TOKEN: " + newTokens.get(i));
+        // }
+
+        for (String name: index.keySet()){
+            String key = name;
+            String value = index.get(name).toString();  
+            System.out.println(key + " " + value.toString());      
+        } 
+        System.out.println("HASH MAP SIZE: " + index.size());
 
     }
 }
