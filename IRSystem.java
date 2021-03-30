e7a38fbc03ec62d16f0ce712fa74a77f6aad2e78
Cameron Costello <c03cost@siena.edu>
2021-03-29 20:56:54 -0400

    Edited IR Main class to test our new tokenizer method that should work when there are quotes in words.

--- IRSystem.java
+++ IRSystem.java
@@ -2,6 +2,7 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.io.IOException;
 import java.io.File;
+import java.util.Scanner;
 /**
  * IRSystem class to run methods of the projects. This is the main class.
  *
@@ -12,32 +13,50 @@ public class IRSystem {
     public static void main(String[] args) throws FileNotFoundException, IOException {
         //The folder containing the training data
         String directory = "TrainingData/";
+        Scanner sc = new Scanner(System.in);
+        
+        String booleanAND = "AND";
+        String booleanOR = "OR";
+        String booleanNOT = "NOT";
+        
         //Creating objects for use in the IR System
         Parser parser = new Parser();
         ArrayList<Document> docs = new ArrayList<Document>();
         Tokenization tokens = new Tokenization();
         ArrayList<String> newTokens = new ArrayList<String>();
         InvertedIndex index = new InvertedIndex();
+        
+        // String testString = "\"to be or not to be\" and colors";
+        // ArrayList<String> testTokens =  tokens.tokenizeQuery(testString);
+        // for (int i = 0; i < testTokens.size(); i++) {
+            // System.out.println(testTokens.get(i));
+        // }
         //Obtains a list of the files in the training data folder
         File directoryPath = new File(directory);
         File filesList[] = directoryPath.listFiles();
         int fileNum = 0;
+        
         //Loop through the list of files to parse their contents and create Document objects
         for (File file: filesList){
             System.out.println("filename: "+ file.toString());
-            docs = parser.trecParser(file.toString());
+            parser.trecParser(file.toString());
             fileNum++;
             System.out.println("File Num: " + fileNum);
         }
+        docs = Parser.docs;
+        
         System.out.println("Number of Docs: " + docs.size());
+        
         //Loop through the document objects to tokenize and add tokens to the inverted index
-        for (int i = 0; i < docs.size(); i++) {
+        for (int i = 0; i < 100; i++) {
             newTokens = tokens.tokenize(docs.get(i));
             System.out.println("Tokenizing completed for docNo: + " + i);
             System.out.println("Normalization completed for docNo: + " + i);
-            index = InvertedIndex.createIndex(newTokens, docs.get(i).getDocID());
+            InvertedIndex.createIndex(newTokens, docs.get(i).getDocID());
             System.out.println("Index completed for docNo: + " + i);
         }
+        index = InvertedIndex.index;
+        
         //Loop through the hash map to print its contents
         for (String name: index.keySet()){
             String key = name;
